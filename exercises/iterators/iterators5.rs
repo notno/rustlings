390a6777a220d39e030539d39e1c289be70c0c9d
Nathan Rosquist <nrosquist@gmail.com>
2024-01-14 04:02:46 -0800

    iter

--- exercises/iterators/iterators5.rs
+++ exercises/iterators/iterators5.rs
@@ -34,7 +34,8 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+    filter then count?
+    fold 
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
