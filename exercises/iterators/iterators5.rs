1ecc1f643206998b2d9fdf2e0f5332fbb1ca780e
Nathan Rosquist <nrosquist@gmail.com>
2024-01-14 15:09:13 -0800

    iter/thread

--- exercises/iterators/iterators5.rs
+++ exercises/iterators/iterators5.rs
@@ -10,11 +10,9 @@
 //
 // Make the code compile and the tests pass.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
-#[derive(Clone, Copy, PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
 enum Progress {
     None,
     Some,
@@ -33,9 +31,8 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
-    // map = { "variables1": Complete, "from_str": None, ... }
-    filter then count?
-    fold 
+    // // map = { "variables1": Complete, "from_str": None, ... }
+    map.iter().filter(|(_, &val)| val == value).count()
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -54,7 +51,12 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
-    todo!();
+    let foo = collection
+        .iter()
+        .fold(0, |acc, map| acc + count_iterator(map, value));
+
+    println!("k{:?}", collection);
+    foo
 }
 
 #[cfg(test)]
--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -6,8 +6,6 @@
 // The program should wait until all the spawned threads have finished and
 // should collect their return values into a vector.
 
-// I AM NOT DONE
-
 use std::thread;
 use std::time::{Duration, Instant};
 
@@ -25,12 +23,18 @@ fn main() {
     let mut results: Vec<u128> = vec![];
     for handle in handles {
         // TODO: a struct is returned from thread::spawn, can you use it?
+        // match handle.join() {
+        //     Ok(time) => results.push(time),
+        //     Err(e) => println!("Thread panicked {:?}", e),
+        // }
+        // results.push(handle.join().unwrap());
+        results.push(handle.join().expect("Thread panicked"));
     }
 
     if results.len() != 10 {
         panic!("Oh no! All the spawned threads did not finish!");
     }
-    
+
     println!();
     for (i, result) in results.into_iter().enumerate() {
         println!("thread {} took {}ms", i, result);
